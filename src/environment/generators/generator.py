07c83ed4a8ee35ffa30acc955943ac6f3489769b
Juraj Trappl <juraj.trappl@proton.me>
2023-09-23 21:28:32 +0200

    add abc base class, private prefix for number of loc

--- src/environment/generators/generator.py
+++ src/environment/generators/generator.py
@@ -1,15 +1,15 @@
-"""
-Generates a 2D grid-like environment with Location objects as units of space.
-"""
-
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 from environment.location import Location
 
 
-class Generator:
+class Generator(ABC):
+    """
+    Generates a 2D grid-like environment with Location objects as units of space.
+    """
+    
     def __init__(self, number_of_locations: int):
-        self.number_of_locations = number_of_locations
+        self._number_of_locations = number_of_locations
 
     @abstractmethod
     def generate(self) -> Location:
