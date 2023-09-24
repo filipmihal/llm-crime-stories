9caf7ab0f18ea4a26ce150a739dabfba2afff9b9
Juraj Trappl <juraj.trappl@proton.me>
2023-09-24 19:48:08 +0200

    simplify the bounds to one function

--- src/environment/grid_visualizer.py
+++ src/environment/grid_visualizer.py
@@ -7,43 +7,22 @@ class GridVisualizer:
     """
 
     @staticmethod
-    def get_top_bound(grid: Grid, size: int) -> int:
-        rows = columns = 2 * size + 1
-        for i in range(rows):
-            for j in range(columns):
+    def get_bounds(grid: Grid, size: int) -> (int, int, int, int):
+        top, left, right, bottom = float('inf'), float('inf'), -1, -1
+
+        for i in range(2 * size + 1):
+            for j in range(2 * size + 1):
                 if grid[i][j] is not None:
-                    return i
-    
-    @staticmethod
-    def get_left_bound(grid: Grid, size: int) -> int:
-        rows = columns = 2 * size + 1
-        for j in range(columns):
-            for i in range(rows):
-                if grid[i][j] is not None:
-                    return j
-    
-    @staticmethod
-    def get_right_bound(grid: Grid, size: int) -> int:
-        rows = columns = 2 * size + 1
-        for j in range(columns - 1, -1, -1):
-            for i in range(rows):
-                if grid[i][j] is not None:
-                    return j
-    
-    @staticmethod
-    def get_bottom_bound(grid: Grid, size: int) -> int:
-        rows = columns = 2 * size + 1
-        for i in range(rows - 1, -1, -1):
-            for j in range(columns):
-                if grid[i][j] is not None:
-                    return i
+                    top = min(top, i)
+                    left = min(left, j)
+                    right = max(right, j)
+                    bottom = max(bottom, i)
+
+        return top, left, right, bottom
     
     @staticmethod
     def visualize(grid: Grid, start_location: Location, size: int) -> None:
-        top = GridVisualizer.get_top_bound(grid, size)
-        left = GridVisualizer.get_left_bound(grid, size)
-        right = GridVisualizer.get_right_bound(grid, size)
-        bottom = GridVisualizer.get_bottom_bound(grid, size)
+        top, left, right, bottom = GridVisualizer.get_bounds(grid, size)
 
         for i in range(top, bottom + 1):
             for j in range(left, right + 1):
