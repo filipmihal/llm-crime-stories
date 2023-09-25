32ec56ebf32f1f26ace7812e6051d8a38f2c638b
Juraj Trappl <juraj.trappl@proton.me>
2023-09-25 12:56:04 +0200

    move story  context to its own llm types

--- src/environment/storyteller.py
+++ src/environment/storyteller.py
@@ -1,25 +1,8 @@
 from abc import abstractmethod
-from dataclasses import dataclass
 import re
-from typing import List, Optional
 
 from llm.llama import Llama
-
-@dataclass
-class StoryContext:
-    """
-    Represents a container for a context needed to generate the description of a location.
-    """
-    
-    """
-    Describes the general atmosphere of a story.
-    """
-    atmosphere: List[str]
-    
-    """
-    When something goes wrong e.g. player tries to move in a forbidden direction, this contains the information.
-    """
-    error: Optional[str]
+from llm.types import StoryContext
 
 class Storyteller:
     """
---
+++ src/llm/types.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+@dataclass
+class StoryContext:
+    """
+    Represents a container for a context needed to generate the description of a location.
+    """
+    
+    """
+    Describes the general atmosphere of a story.
+    """
+    atmosphere: List[str]
+    
+    """
+    When something goes wrong e.g. player tries to move in a forbidden direction, this contains the information.
+    """
+    error: Optional[str]
