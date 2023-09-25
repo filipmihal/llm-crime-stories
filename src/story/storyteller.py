5810219464053ffc0a0f11306492595568266fc7
Juraj Trappl <juraj.trappl@proton.me>
2023-09-25 16:51:36 +0200

    update prompt

--- src/story/storyteller.py
+++ src/story/storyteller.py
@@ -23,13 +23,13 @@ class LlamaStoryteller(Storyteller):
     
     def __init__(self) -> None:
         self._question_prompt = f"""
-            Q: Describe a new location's atmosphere.
+            Q: Create a new location and describe its atmosphere.
         """
 
         self._system_prompt = lambda atmosphere: f"""
             S: You are a storyteller in a text-based game.
-            The game has this specific general atmosphere: {', '.join(atmosphere)}.
-            Your task is to describe location atmosphere in a light artistic style using 2-3 sentences.
+            The game has this general atmosphere: {', '.join(atmosphere)}.
+            Your task is to create new locations and describe their atmosphere in a light artistic style using 2-3 sentences while maintaining the general atmosphere.
         """
         
         self._llama = Llama()
