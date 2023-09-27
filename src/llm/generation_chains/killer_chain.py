5e0dcb29d850c0221d9d056d231afc56af08f9d3
Juraj Trappl <juraj.trappl@proton.me>
2023-09-28 01:03:26 +0200

    omg i missed parentheses

--- src/llm/generation_chains/killer_chain.py
+++ src/llm/generation_chains/killer_chain.py
@@ -27,7 +27,7 @@ class KillerYamlOutputParser(BaseOutputParser):
 
         obj = yaml.safe_load(group)
         try:
-            return KillerSchema.load(data=obj['killer'])
+            return KillerSchema().load(obj['killer'])
         except ValidationError as err:
             print(err.messages)
             return None
