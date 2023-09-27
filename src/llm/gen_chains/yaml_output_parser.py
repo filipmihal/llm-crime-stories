b434bf42e1d3f046291b7b084c248d25352047d2
Juraj Trappl <juraj.trappl@proton.me>
2023-09-27 18:42:20 +0200

    add yaml output parser

---
+++ src/llm/gen_chains/yaml_output_parser.py
@@ -0,0 +1,11 @@
+import yaml
+from langchain.schema import BaseOutputParser
+
+
+class YamlOutputParser(BaseOutputParser):
+    """Parse the output of an LLM call to YAML."""
+
+    def parse(self, text: str):
+        """Parse the output of an LLM call."""
+        return yaml.safe_load(text)
+        
