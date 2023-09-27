012e6710e2e990886da53e33d71dfe866684a5a9
Juraj Trappl <juraj.trappl@proton.me>
2023-09-27 20:38:57 +0200

    debug print for text to parse into yaml

--- src/llm/gen_chains/yaml_output_parser.py
+++ src/llm/gen_chains/yaml_output_parser.py
@@ -7,6 +7,7 @@ class YamlOutputParser(BaseOutputParser):
 
     def parse(self, text: str):
         """Parse the output of an LLM call."""
+        print(text)
         yaml_in_text = re.search(r'- victim:[\s\S]*', text).group(0)
         return yaml.safe_load(yaml_in_text)
         
