a47a088f10454c184bd9f1c29be3261d49094d65
Juraj Trappl <juraj.trappl@proton.me>
2023-09-27 22:48:17 +0200

    divide suspects to killer and suspect one after another

--- src/llm/gen_chains/suspect_chain.py
+++ src/llm/gen_chains/suspect_chain.py
@@ -3,6 +3,7 @@ from langchain.schema import BaseOutputParser
 import re
 import yaml
 
+
 class SuspectYamlOutputParser(BaseOutputParser):
     """Parse the output of an LLM call of the Suspect chain to YAML."""
 
@@ -16,26 +17,49 @@ class SuspectYamlOutputParser(BaseOutputParser):
             or re.search(r"[sS]uspects:[\s\S]*\n", text)
         )
         group = match.group(0)
-        
-        if '`' in group:
-            group = re.search(r'([^`]+)`', group).group(1).strip()
-        
+
+        if "`" in group:
+            group = re.search(r"([^`]+)`", group).group(1).strip()
+
         return yaml.safe_load(group)
+
+
+class KillerYamlOutputParser(BaseOutputParser):
+    """Parse the output of an LLM call of the Killer chain to YAML."""
+
+    def parse(self, text: str):
+        """Parse the output of an LLM call."""
+        print(text)
+        match = (
+            re.search(r"- [kK]iller:[\s\S]*", text)
+            or re.search(r"[kK]iller:[\s\S]*", text)
+            or re.search(r"- [kK]iller:[\s\S]*\n", text)
+            or re.search(r"[kK]iller:[\s\S]*\n", text)
+        )
+        group = match.group(0)
+
+        if "`" in group:
+            group = re.search(r"([^`]+)`", group).group(1).strip()
+
+        return yaml.safe_load(group)
+
 
 class SuspectChain:
     def __init__(self, llm):
-        self._prompt = PromptTemplate.from_template(
+        self._llm = llm
+
+        self._suspect_prompt = PromptTemplate.from_template(
             """
             <s>[INST] <<SYS>>
             You are a crime storyteller.
             <<SYS>>
 
-            Given a theme and information about the victim describe 3 suspects, one of whom is the killer.
+            Given a theme and information about the victim describe a suspect that is not a killer.
             
-            An example is below. Note that do not output anything else other than just the YAML of created suspects.
+            An example is below. Note that do not output anything else other than just the YAML of a created suspect.
             
             Example
-            suspects: 
+            suspect: 
               - name: "Lucius"
                 age: 35
                 occupation: "Librarian's Assistant"
@@ -44,30 +68,46 @@ class SuspectChain:
                   Several witnesses saw him there throughout the evening.
                 motive: >
                   Lucius had a longstanding feud with Drusilla, who constantly criticized his work and suggested he was not fit for his role.
-                  He might have wanted to silence her.
-                is_guilty: false
-              - name: "Aelia"
-                age: 30
-                occupation: "Fellow Scholar"
-                alibi: >
-                  Aelia says she was conducting research in a separate section of the library. Her colleagues confirm that she was engrossed in her work and didn't leave her area.
-                motive: >
-                  Aelia had discovered a hidden manuscript that could have elevated her status in the scholarly community. Drusilla knew about this discovery and might have wanted to steal her thunder.
-                is_guilty: false
-              - name: "Gaius"
-                age: 40
-                occupation: "Crazy Librarian"
-                alibi: >
-                  Gaius, known as the 'Crazy Librarian,' has no alibi. He claims he was in his secret chamber, delving into forbidden texts. No one can vouch for his whereabouts.
-                motive: >
-                  Gaius had become increasingly obsessed with ancient and forbidden knowledge. He believed that by eliminating anyone who questioned him, he could protect the library's secrets.
-                is_guilty: true [/INST]
+                  He might have wanted to silence her. [/INST]
                                     
             Give the output for the following theme: {theme} and victim information: {victim}
             """
         )
 
-        self._chain = self._prompt | llm | SuspectYamlOutputParser()
+        self._killer_prompt = PromptTemplate.from_template(
+            """
+            <s>[INST] <<SYS>>
+            You are a crime storyteller.
+            <<SYS>>
+
+            Given a theme and information about the victim describe a killer.
+            
+            An example is below. Note that do not output anything else other than just the YAML of a created killer.
+            
+            Example
+            killer: 
+              - name: "Gaius"
+                age: 40
+                occupation: "Crazy Librarian"
+                alibi: >
+                  Gaius, known as the 'Crazy Librarian,' has no alibi. He claims he was in his secret chamber, delving into forbidden texts.
+                  No one can vouch for his whereabouts.
+                motive: >
+                  Gaius had become increasingly obsessed with ancient and forbidden knowledge.
+                  He believed that by eliminating anyone who questioned him, he could protect the library's secrets. [/INST]
+                                    
+            Give the output for the following theme: {theme} and victim information: {victim}
+            """
+        )
 
     def create(self, theme, victim):
-        return self._chain.invoke({"theme": theme, "victim": victim})
+        suspects = []
+
+        self._killer_chain = self._killer_prompt | self._llm | KillerYamlOutputParser()
+        suspects.append(self._killer_chain.invoke({"theme": theme, "victim": victim}))
+
+        self._suspect_chain = self._prompt | self._llm | SuspectYamlOutputParser()
+        for _ in range(2):
+          suspects.append(self._suspect_chain.invoke({"theme": theme, "victim": victim}))
+
+        return suspects
