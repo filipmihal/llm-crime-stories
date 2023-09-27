a68570a9a00821f5db7bd77bb21855167a3fd858
Juraj Trappl <juraj.trappl@proton.me>
2023-09-27 13:25:45 +0200

    add suspect chain

---
+++ src/llm/gen_chains/suspect_chain.py
@@ -0,0 +1,48 @@
+from llm.gen_chains.json_output_parser import JsonOutputParser
+
+class SuspectChain:
+    def __init__(self, llm):
+        self._json_schema = {
+          "$schema": "http://json-schema.org/draft-07/schema#",
+          "title": "Generated schema for Root",
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "age": {
+              "type": "number"
+            },
+            "occupation": {
+              "type": "string"
+            },
+            "alibi": {
+              "type": "string"
+            },
+            "motivation": {
+              "type": "string"
+            },
+            "is_guilty": {
+              "type": "boolean"
+            }
+          },
+          "required": [
+            "name",
+            "age",
+            "occupation",
+            "alibi",
+            "motivation",
+            "is_guilty"
+          ]
+        }
+        
+        self._prompt = """
+            You are a crime storyteller. The theme of the story is: {{theme}}.
+            The victim: {{victim}}. Describe 3 suspects, one of whom is the killer.
+            Return the response in this json schema: {{schema}}
+        """
+        
+        self._chain = self._prompt | llm | JsonOutputParser()
+    
+    def create(self, theme, victim):     
+        return self._chain.invoke({ "theme": theme, "victim": victim, "schema": self._schema })
