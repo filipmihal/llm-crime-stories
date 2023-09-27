edc354a0b6e397fbf3284d807db5a93b98879a72
Juraj Trappl <juraj.trappl@proton.me>
2023-09-27 12:46:32 +0200

    create llm story gen

---
+++ src/llm/llm_story_generator.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+import json
+from typing import List
+
+@dataclass
+class Room:
+    name: str
+    description: str
+
+@dataclass
+class Suspect:
+    name: str
+    age: int
+    occupation: str
+    is_guilty: bool
+    motive: str
+    alibi: str
+
+@dataclass
+class Victim:
+    name: str
+    age: int
+    occupation: str
+    murder_weapon: str
+    death_description: str
+
+@dataclass
+class Story:
+    theme: str
+    victim: Victim
+    suspects: List[Suspect]
+    rooms: List[Room]
+
+class LlmStoryGenerator:
+    def __init__(self):
+        pass
+    
+    def create_new_story(self, dummy: bool = False) -> Story:
+        if dummy:
+            with open('../../data/dummy.json', 'r') as f:
+                return json.loads(f)
+    
+        # complex llm logic with langchain
+    
+        return {}
