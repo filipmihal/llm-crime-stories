93e5fd86c51621f850024a4cf811822c65846bdf
Juraj Trappl <juraj.trappl@proton.me>
2023-09-27 13:26:34 +0200

    add output parser

---
+++ src/llm/gen_chains/json_output_parser.py
@@ -0,0 +1,10 @@
+import json
+from langchain.schema import BaseOutputParser
+
+class JsonOutputParser(BaseOutputParser):
+    """Parse the output of an LLM call to a JSON."""
+
+
+    def parse(self, text: str):
+        """Parse the output of an LLM call."""
+        return json.loads(text)
---
+++ src/llm/gen_chains/rooms_chain.py
@@ -0,0 +1,9 @@
+from llm.gen_chains.json_output_parser import JsonOutputParser
+
+class RoomsChain:
+    def __init__(self, llm, rooms_layout):
+        self._llm = llm
+        self._rooms_layout
+        
+    def create(self, theme, victim, suspects):
+        pass
