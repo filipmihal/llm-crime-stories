4b09026206e4fa503232e4041c79cd70269c33a9
Juraj Trappl <juraj.trappl@proton.me>
2023-09-27 20:20:24 +0200

    trying robust few shot

--- src/llm/gen_chains/victim_chain.py
+++ src/llm/gen_chains/victim_chain.py
@@ -8,19 +8,30 @@ class VictimChain:
         self._prompt = PromptTemplate.from_template(
             """
             <s>[INST] <<SYS>>
-            You are a crime storyteller. When answering, don't use pre-amble. Just return the result converted to YAML.
+            You are a crime storyteller.
             <<SYS>>
 
-            Create a victim based on the theme: {{theme}}. Give its name, age, occupation, murder weapon and death description.
+            Given a theme create a victim in a fictional crime story. Victim is described by its name, age, occupation, murder weapon and death description.
+            A couple of examples are below. Note: you are to output just YAML of the created victim.
             
-            An example of a desired output for a created victim:
-            
+            Example 1)
             - victim:
                 name: "Alicia Williams"
                 age: 25
                 occupation: "nurse"
                 murder_weapon: "hunter's knife"
                 death_description: "Body lying in blood on the kitchen's floor, stabbed 36 times in the body area"
+                
+            Example 2)
+            - victim:
+                name: "Bradley Johnson"
+                age: 42
+                occupation: "lawyer"
+                murder_weapon: "handgun"
+                death_description: "Body found in the garage with one bullet wound to the chest, signs of strangulation"
+            
+            Give the output for the following theme:
+            {{theme}}
             """
         )
 
