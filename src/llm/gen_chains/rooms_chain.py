2eff3aaefe7ef0cd3e0f66b3788b62bed14ef64a
Juraj Trappl <juraj.trappl@proton.me>
2023-09-27 15:46:22 +0200

    missing type

--- src/llm/gen_chains/rooms_chain.py
+++ src/llm/gen_chains/rooms_chain.py
@@ -46,7 +46,7 @@ class RoomsChain:
         middle_row, middle_col = self._rows // 2 + 1, self._columns // 2 + 1
         start_story = self.generate_room(self._victim_prompt, theme, victim)
         # start_story.update({"row": middle_row, "col": middle_col})
-        start_story += "row:" + middle_row + ", col:" + middle_col
+        start_story += "row:" + str(middle_row) + ", col:" + str(middle_col)
 
         # contains final description of rooms
         rooms_data, suspects_positions = [start_story], []
@@ -66,7 +66,7 @@ class RoomsChain:
                 self._suspect_prompt, theme, not_selected_suspects.popleft()
             )
             #current_room_story.update({"row": row, "j": col})
-            current_room_story += "row:" + row + ", col:" + col
+            current_room_story += "row:" + str(row) + ", col:" + str(col)
             rooms_data.append(current_room_story)
 
             generated.add((row, col))
