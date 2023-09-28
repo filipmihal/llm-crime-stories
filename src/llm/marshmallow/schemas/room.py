02617f633a7b90de1e966082a5445e470716ec5e
Juraj Trappl <juraj.trappl@proton.me>
2023-09-28 09:44:46 +0200

    row col are firstly optional

--- src/llm/marshmallow/schemas/room.py
+++ src/llm/marshmallow/schemas/room.py
@@ -4,5 +4,5 @@ from marshmallow import Schema, fields
 class RoomSchema(Schema):
     name = fields.Str(required=True)
     description = fields.Str(required=True)
-    row = fields.Integer(required=True)
-    col = fields.Integer(required=True)
+    row = fields.Integer()
+    col = fields.Integer()
