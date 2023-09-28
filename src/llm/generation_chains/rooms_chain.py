d1170b8c8120d33ade070b8c6bbb551cc0f21ac9
Juraj Trappl <juraj.trappl@proton.me>
2023-09-28 09:18:20 +0200

    rename j to col

--- src/llm/generation_chains/rooms_chain.py
+++ src/llm/generation_chains/rooms_chain.py
@@ -105,7 +105,7 @@ class RoomsChain:
             current_room_story = self.generate_room(
                 self._suspect_prompt, theme, json.dumps(not_selected_suspects.popleft())
             )
-            current_room_story.update({"row": row, "j": col})
+            current_room_story.update({"row": row, "COL": col})
             rooms_data.append(current_room_story)
 
             generated.add((row, col))
