9ab6b9e4e0072b55ca929d526892d6eeac4dbdd3
Juraj Trappl <juraj.trappl@proton.me>
2023-09-28 11:25:01 +0200

    shorten package name #2

--- src/llm/chains/__init__.py
+++ src/llm/chains/__init__.py
@@ -1,6 +1,6 @@
-from llm.generation_chains.killer_chain import KillerChain
-from llm.generation_chains.rooms_chain import RoomsChain
-from llm.generation_chains.suspect_chain import SuspectChain
-from llm.generation_chains.victim_chain import VictimChain
+from llm.chains.killer_chain import KillerChain
+from llm.chains.rooms_chain import RoomsChain
+from llm.chains.suspect_chain import SuspectChain
+from llm.chains.victim_chain import VictimChain
 
 __all__ = ["KillerChain", "RoomsChain", "SuspectChain", "VictimChain"]
