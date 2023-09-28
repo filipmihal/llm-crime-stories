37f609fa015a9fe4979102fc4cd65179a69d82ee
Juraj Trappl <juraj.trappl@proton.me>
2023-09-28 09:27:33 +0200

    add missing required, add row and col attr

--- src/llm/marshmallow/schemas/killer.py
+++ src/llm/marshmallow/schemas/killer.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields, validate
 
 class KillerSchema(Schema):
     name = fields.Str(required=True)
-    age = fields.Integer(validate=validate.Range(min=0))
+    age = fields.Integer(required=True, validate=validate.Range(min=0))
     occupation = fields.Str(required=True)
     motive = fields.Str(required=True)
     alibi = fields.Str(required=True)
--- src/llm/marshmallow/schemas/room.py
+++ src/llm/marshmallow/schemas/room.py
@@ -4,3 +4,5 @@ from marshmallow import Schema, fields
 class RoomSchema(Schema):
     name = fields.Str(required=True)
     description = fields.Str(required=True)
+    row = fields.Integer(required=True)
+    col = fields.Integer(required=True)
