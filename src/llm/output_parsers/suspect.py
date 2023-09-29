474d0b24379e7960e1b158d965cda8e1640a4d10
Juraj Trappl <juraj.trappl@proton.me>
2023-09-29 16:10:44 +0200

    fix suspect output parser json name

--- src/llm/output_parsers/suspect.py
+++ src/llm/output_parsers/suspect.py
@@ -6,7 +6,7 @@ from typing import Optional
 from llm.marshmallow.schemas.suspect import SuspectSchema
 
 
-class SuspectJSONOutputParser(BaseOutputParser):
+class SuspectJsonOutputParser(BaseOutputParser):
     """Parse the output of an LLM call of the Suspect chain to JSON."""
 
     def parse(self, text: str) -> Optional[SuspectSchema]:
