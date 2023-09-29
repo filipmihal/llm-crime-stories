d4d37f8bffcf75781f92ab452fcad4ac8ecd9489
Juraj Trappl <juraj.trappl@proton.me>
2023-09-29 12:45:13 +0200

    correct killer chain template

--- src/llm/chains/killer_chain.py
+++ src/llm/chains/killer_chain.py
@@ -9,26 +9,28 @@ class KillerChain:
         self._killer_prompt = PromptTemplate.from_template(
             """
             <s>[INST] <<SYS>>
+            
             You are a crime storyteller.
+            
             <<SYS>>
 
-            Given a theme and information about the victim describe a killer.
+            Given a theme: "Library of Alexandria, 340 BC, crazy librarian" and victim information: "{'name': 'Archibald Ptolemy', 'age': 35, 'occupation': 'Head Librarian', 'murder_weapon': 'Ancient scroll with poisoned ink', 'death_description': 'Found dead in his office surrounded by stacks of books, face contorted in a mixture of fear and surprise, as if he had been reading a particularly gruesome text when struck down.'}" describe a killer.
+            killer:
+            [/INST]
+            name: "Gaius"
+            age: 40
+            occupation: "Crazy Librarian"
+            alibi: >
+              Gaius, known as the 'Crazy Librarian,' has no alibi. He claims he was in his secret chamber, delving into forbidden texts.
+              No one can vouch for his whereabouts.
+            motive: >
+              Gaius had become increasingly obsessed with ancient and forbidden knowledge.
+              He believed that by eliminating anyone who questioned him, he could protect the library's secrets.</s><s>
             
-            An example is below. Note that you must output only created killer converted to YAML where all properties are on same level.
-            
-            Example
-            killer: 
-              name: "Gaius"
-              age: 40
-              occupation: "Crazy Librarian"
-              alibi: >
-                Gaius, known as the 'Crazy Librarian,' has no alibi. He claims he was in his secret chamber, delving into forbidden texts.
-                No one can vouch for his whereabouts.
-              motive: >
-                Gaius had become increasingly obsessed with ancient and forbidden knowledge.
-                He believed that by eliminating anyone who questioned him, he could protect the library's secrets. [/INST]
-                                    
-            Give the output for the following theme: {theme} and victim information: {victim}
+            [INST]
+            Given a theme: {theme} and victim information: {victim} describe a killer.
+            killer:
+            [/INST]
             """
         )
         
