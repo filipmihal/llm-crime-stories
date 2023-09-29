e6ea60ee1a7bc9f26a729f83054bca4d918d0371
Juraj Trappl <juraj.trappl@proton.me>
2023-09-29 18:03:35 +0200

    fix suspects output parsing from single object to an array of objects

--- src/llm/output_parsers/suspect.py
+++ src/llm/output_parsers/suspect.py
@@ -2,7 +2,7 @@ import json
 from json.decoder import JSONDecodeError
 from langchain.schema import BaseOutputParser
 from marshmallow import ValidationError
-from typing import Optional
+from typing import List, Optional
 
 from llm.marshmallow.schemas.suspect import SuspectSchema
 
@@ -12,16 +12,16 @@ class SuspectJsonOutputParser(BaseOutputParser):
     Parse the output of an LLM call of the Suspect chain to JSON.
     """
 
-    def parse(self, text: str) -> Optional[SuspectSchema]:
+    def parse(self, text: str) -> Optional[List[SuspectSchema]]:
         """
         Parse the output of an LLM call.
         """
         print("suspects" + text)
         try:
             obj = json.loads(text)
-            obj = {k.strip():v.strip() for k, v in obj.items()}
+            obj = [{k.strip():v.strip() for k, v in o.items()} for o in obj]
             
-            return SuspectSchema().load(obj)
+            return [SuspectSchema().load(o) for o in obj]
         except JSONDecodeError as decode_err:
             print(decode_err)
         except ValidationError as err:
