400bce2de0d881d0776baa7f0093dfb0c99cbe97
Juraj Trappl <juraj.trappl@proton.me>
2023-09-29 22:08:24 +0200

    remove motives from schema

--- src/llm/marshmallow/schemas/killer.py
+++ src/llm/marshmallow/schemas/killer.py
@@ -5,5 +5,4 @@ class KillerSchema(Schema):
     name = fields.Str(required=True)
     age = fields.Integer(required=True, validate=validate.Range(min=0))
     occupation = fields.Str(required=True)
-    motive = fields.Str(required=True)
     alibi = fields.Str(required=True)
